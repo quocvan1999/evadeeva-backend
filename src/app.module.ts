5f8934f567d34cbdcf4963d7ededac36d2a4e9b7
Quocvan <lyquocvan1999@gmail.com>
2025-01-02 19:59:25 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cookieParser());
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Api EvaDeEva')
+    .setDescription('Danh sách Api Eva de eva')
+    .setVersion('1.0')
+    .build();
+
+  const swagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('swagger', app, swagger);
+
+  await app.listen(process.env.PORT ?? port);
+}
+bootstrap();
