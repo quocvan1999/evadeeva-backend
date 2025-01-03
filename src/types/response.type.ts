3d38451393ba5c72243c10be4cdc40dbca49be57
Quocvan <lyquocvan1999@gmail.com>
2025-01-03 21:35:53 +0700

    push foder src/types/response.type.ts

--- src/types/response.type.ts
+++ src/types/response.type.ts
@@ -1,22 +1,4 @@
-export type ResponseSuccessType<T> = {
-  statusCode: number;
-  content: {
-    message: string;
-    data?: T;
-  };
-  timestamp: Date;
-};
-
-export type ResponseErrorType<T> = {
-  statusCode: number;
-  content: {
-    message: string;
-    error: T;
-  };
-  timestamp: Date;
-};
-
 export type ResponseType<T> = {
   statusCode: number;
-  data: T;
+  data?: T;
 };
